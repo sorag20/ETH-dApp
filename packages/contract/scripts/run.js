0e3209612af70586390d50c7b21f87c603a59728
sorag <gkthn20@gmail.com>
2023-06-15 23:36:52 +0900

    finish

--- packages/contract/scripts/run.js
+++ packages/contract/scripts/run.js
@@ -1,34 +1,80 @@
-const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
-  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
-  const wavePortal = await waveContract.deployed();
+// SPDX-License-Identifier: MIT
 
-  console.log("Contract deployed to:", wavePortal.address);
-  console.log("Contract deployed by:", owner.address);
+pragma solidity ^0.8.17;
 
-  let waveCount;
-  waveCount = await waveContract.getTotalWaves();
+import "hardhat/console.sol";
 
-  let waveTxn = await waveContract.wave();
-  await waveTxn.wait();
+contract WavePortal {
+    uint256 totalWaves;
+    uint256 private seed;
 
-  waveCount = await waveContract.getTotalWaves();
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
-  waveTxn = await waveContract.connect(randomPerson).wave();
-  await waveTxn.wait();
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
 
-  waveCount = await waveContract.getTotalWaves();
-};
+    Wave[] waves;
 
-const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
+    /*
+     * "address => uint mapping"は、アドレスと数値を関連付ける
+     */
+    mapping(address => uint256) public lastWavedAt;
 
-runMain();
+    constructor() payable {
+        console.log("We have been constructed!");
+        /*
+         * 初期シードの設定
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        /*
+         * 現在ユーザーがwaveを送信している時刻と、前回waveを送信した時刻が15分以上離れていることを確認。
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * ユーザーの現在のタイムスタンプを更新する
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         *  ユーザーのために乱数を設定
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than they contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        return totalWaves;
+    }
+}
